f63fdbaa4f8b2811af00931599455b3b99cdf5c2
Raphaël Bournhonesque <raphael@bournhonesque.eu>
2019-11-11 22:20:11 +0100

    Add safety check to typos in extra parameters

--- utils/__init__.py
+++ utils/__init__.py
@@ -2,13 +2,13 @@ import json
 from typing import Dict, List, Tuple
 
 
-def update_dict_dot(data: Dict, update: str):
+def update_dict_dot(data: Dict, update: str, allow_insertion: bool = False):
     """Update a dictionary using javascript dot notation style.
     """
     parsed_update = parse_update(update)
 
     for key, value in parsed_update:
-        update_dict_dot_single(data, key, value)
+        update_dict_dot_single(data, key, value, allow_insertion)
 
 
 def parse_update(update_query: str) -> List[Tuple[str, object]]:
@@ -23,11 +23,14 @@ def parse_update(update_query: str) -> List[Tuple[str, object]]:
     return updates
 
 
-def update_dict_dot_single(data: Dict, key: str, value):
+def update_dict_dot_single(data: Dict, key: str, value, allow_insertion: bool):
     key_splitted: List[str] = key.split('.')
 
     data_ = data
     for i, sub_key in enumerate(key_splitted):
+        if sub_key not in data_ and not allow_insertion:
+            raise ValueError("invalid key: {}, unknown sub key: {}".format(key, sub_key))
+
         if i == len(key_splitted) - 1:
             data_[sub_key] = value
         else:
