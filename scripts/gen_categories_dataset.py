49d27f77f5a96f41b4b4c29cd41e49ada824aaaa
Alex Garel <alex@garel.org>
2022-04-13 11:35:01 +0200

    fix: fixes to gen_categories_dataset

--- scripts/gen_categories_dataset.py
+++ scripts/gen_categories_dataset.py
@@ -13,6 +13,7 @@ import os.path
 import random
 import re
 import tarfile
+import textwrap
 import time
 from datetime import datetime, timedelta
 
@@ -72,8 +73,23 @@ class DataPathes:
     def images_file(self):
         return self.target_dir + "/" + self.files_prefix + "_images.tar"
 
+    @property
+    def documentation_file(self):
+        return self.target_dir + "/" + self.files_prefix + "_documentation.md"
+
 
 class IdsSelector:
+
+    # template for documentation. See generates_documentation
+    DOCUMENTATION = """
+    This dataset contains {total} product, that were randomly selected to contains, roughly:
+
+    - {kind_sizes[popular]} popular products
+    - {num_each_agri} products for each category linked to agribalyse
+    - {kind_sizes[misc_agri]} products with various categories linked to agribalyse
+    - {kind_sizes[other]} other products
+    """
+    documentation = "Not generated"  # we will generate from DOCUMENTATION when we have quantities
 
     # this is the target we have
     quantities = {
@@ -113,13 +129,18 @@ class IdsSelector:
             value = self._caches["agribalyse_cats"]
         except KeyError:
             value = set(cat for cat in self.agribalyse_categories.keys())
-            self._caches["agribalyse_cats"] = value  # hack: cache
+            self._caches["agribalyse_cats"] = value
         return value
 
     def product_agribalyse(self, product):
         """Get categories of a product that have an agribalyse"""
         cats = set(product.get("categories_tags", [])) & self.agribalyse_cats
         return cats
+
+    @property
+    def num_each_agri(self):
+        """number of ids to select for each category related to agrybalise"""
+        return self.quantities["total"] * self.quantities["each_agribalyse"] / len(self.agribalyse_cats)
 
     @property
     def kind_sizes(self):
@@ -131,14 +152,14 @@ class IdsSelector:
             kind_sizes = self._caches["kind_sizes"]
         except KeyError:
             # total elements we want by kind
-            num_agri = self.quantities["total"] * self.quantities["each_agribalyse"] / len(self.agribalyse_cats)
+            num_agri = self.num_each_agri
             num_misc_agri = self.quantities["total"] * self.quantities["misc_agribalyse"]
             num_popular = self.quantities["total"] * self.quantities["many_scans"]
             num_other = self.quantities["total"] - num_agri - num_misc_agri - num_popular
             kind_sizes = [("popular", num_popular), ("misc_agri", num_misc_agri), ("other", num_other)]
             kind_sizes.extend((cat, num_agri) for cat in self.agribalyse_categories)
             kind_sizes = {k: int(v) for k, v in kind_sizes if v >= 1}
-            self._caches["kind_sizes"] = kind_sizes  # hack: cache
+            self._caches["kind_sizes"] = kind_sizes
         return kind_sizes
 
     def select_ids(self):
@@ -165,7 +186,7 @@ class IdsSelector:
             """
             # I'm really not 100% sure my methodology is fair against ids !
             # It has the advantage of being streamed and not taking much memory
-            # and also to immediatly decide if we retain an id or not !
+            # and also to immediately decide if we retain an id or not !
             # I made an empirical validation of simple case (weight=1) here:
             # https://gist.github.com/alexgarel/9de2f548414ddeea1ed5279800d98ddd
             seen = ids_seen[kind] + weight
@@ -217,15 +238,36 @@ class IdsSelector:
                     kind=kind, size=len(kind_ids), wanted=kind_sizes[kind]
                 ))
 
-    def ids_by_kind(self):
-        """
+    def generates_documentation(self, ids):
+        """Generates the documentation string"""
+        self.documentation = self.DOCUMENTATION.format(
+            kind_sizes=self.kind_sizes,
+            total=sum(len(v) for v in ids.values()),
+            num_each_agri=self.num_each_agri,
+        )
+        # remove first empty line and dedent
+        self.documentation = textwrap.dedent(self.documentation[1:])
+        with open(self.data_pathes.documentation_file, "w") as f:
+            f.write(self.documentation)
+
+    def ids_by_kind(self, ids_path=None):
+        """try to loads ids from ids_path or generate them
+
         :return dict: ids list by kind
         """
-        # try to be reproducible
-        random.seed(self.seed)
-        ids = self.select_ids()
-        self.check_ids(ids)
-        return ids
+        if ids_path and not ids_path.endswith("json.gz"):
+            raise AssertionError("ids path must end with json.gz")
+        if ids_path and os.path.exists(ids_path):
+            ids_by_kind = json.load(gzip.open(ids_path, "rt"))
+        else:
+            # try to be reproducible
+            random.seed(self.seed)
+            ids_by_kind = self.select_ids()
+            if ids_path:
+                json.dump(ids_by_kind, gzip.open(ids_path, "wt"))
+        self.check_ids(ids_by_kind)
+        self.generates_documentation(ids_by_kind)
+        return ids_by_kind
 
     def all_ids(self, ids_by_kind):
         """all ids as a single sorted list (sorted by line number)
@@ -235,6 +277,37 @@ class IdsSelector:
 
 class DataHarvester:
     """Harvest data corresponding to certain ids"""
+
+    # documentation template, see documentation property
+    DOCUMENTATION = """
+
+    - products file: {products_file},
+      a jsonl, contains data about each product as a dict.
+      Available fields are:
+      - code: product barcode
+      - categories_tags: normalized categories
+      - categories_hierarchy: list of ordered categories from parent to children
+      - categories_properties: properties computed from categories
+      - product_name_xx: product name in language xx
+      - nutriments: data about nutriments
+      - ingredients_xx: parsed ingredients in language xx
+      - ingredients_text_xx: original text for ingredients in language xx
+      - images is a dict by image language code (or xx if no language code),
+        which in turns contains dict by image kind, (front, ingredients, packaging, other_1..n)
+        which contains data about an image.
+
+    - ocrs file: {ocrs_file},
+      contains a line per product, in same order as products file.
+      - code: product barcode
+      - ocrs: has the same structure as `images` in products file,
+        but contains ocr data (as returned by Google vision) instead of image data
+      - no_ocr: returns images id for which we did not find any OCR
+
+    - images file: {images_file},
+      is a tar archive with all images contained in products file `images files`.
+      Each file has path barcode/imgid.jpg.
+      Images are all 400 px width images.
+    """
 
     max_image_monthes = 12  # max age in monthes
     image_size = "400"  # image size, if None, takes original
@@ -287,7 +360,7 @@ class DataHarvester:
             recent = int(image.get("uploaded_t") or 0) > min_ts
             if is_selected or recent:
                 if not is_selected:
-                    kind = "other"
+                    kind = "other_%s" % image["imgid"]
                     lang = "xx"
                     # add imageid
                     image["imgid"] = key
@@ -296,6 +369,16 @@ class DataHarvester:
                     # FIXME: not sure, maybe there is a default lang ?
                     lang = lang[0] if lang else "xx"
                 images[lang][kind] = image
+        # remove images in "other" that are already in a "selected" kind
+        images_xx = images["xx"]  # "other" is only in "xx"
+        for lang, lang_data in images.items():
+            for kind, image in lang_data.items():
+                if not kind.startswith("other"):
+                    try:
+                        del images_xx["other_%s" % image["imgid"]]
+                    except KeyError:
+                        pass  # was not selected, not a problem
+
         return images
 
     def images_ocr(self, product, images_data):
@@ -309,11 +392,15 @@ class DataHarvester:
                 )
                 # remove extension
                 path = path.rsplit(".", 1)[0]
-                if os.path.exists(path + ".json"):
-                    ocr_data[lang][kind] = json.load(open(path + ".json"))
-                elif os.path.exists(path + ".json.gz"):
-                    ocr_data[lang][kind] = json.load(gzip.open(path + ".json.gz", "rt"))
-                else:
+                try:
+                    if os.path.exists(path + ".json"):
+                        ocr_data[lang][kind] = json.load(open(path + ".json"))
+                    elif os.path.exists(path + ".json.gz"):
+                        ocr_data[lang][kind] = json.load(gzip.open(path + ".json.gz", "rt"))
+                    else:
+                        no_ocr.append(image["imgid"])
+                except json.JSONDecodeError:
+                    # this happens, once in a while…
                     no_ocr.append(image["imgid"])
         return {"code": code, "ocrs": ocr_data, "no_ocr": no_ocr}
 
@@ -361,9 +448,21 @@ class DataHarvester:
                 break
             next_line, code = ids.pop()
 
+    @property
+    def documentation(self):
+        documentation = self.DOCUMENTATION.format(
+            products_file=os.path.basename(self.data_pathes.products_file),
+            ocrs_file=os.path.basename(self.data_pathes.ocrs_file),
+            images_file=os.path.basename(self.data_pathes.images_file),
+        )
+        # remove first empty line and dedent
+        return textwrap.dedent(documentation[1:])
+
     def __call__(self, ids):
         # select data from mongo dump
         start = time.monotonic()
+        with open(self.data_pathes.documentation_file, "a") as f:
+            f.write(self.documentation)
         with gzip.open(self.data_pathes.products_file, "wt") as products, \
                 gzip.open(self.data_pathes.ocrs_file, "wt") as ocrs, \
                 tarfile.open(self.data_pathes.images_file, "w") as images:
@@ -383,14 +482,7 @@ class DataHarvester:
 def generate_data():
     start = time.monotonic()
     ids_selector = IdsSelector()
-    if os.path.exists("_ids_by_kind.json.gz"):
-        print("Loading ids")
-        ids_by_kind = json.load(gzip.open("_ids_by_kind.json.gz", "rt"))
-        ids_selector.check_ids(ids_by_kind)
-    else:
-        print("Selecting ids")
-        ids_by_kind = ids_selector.ids_by_kind()
-        json.dump(ids_by_kind, gzip.open("_ids_by_kind.json.gz", "wt"))
+    ids_by_kind = ids_selector.ids_by_kind("_ids_by_kind.json.gz")
     ids = ids_selector.all_ids(ids_by_kind)
     ids_time = time.monotonic()
     print("Got", len(ids), "ids in", "%.0f seconds" % (ids_time - start))
