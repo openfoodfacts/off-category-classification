7c90c19809609b5025a4e29c81b123dc79b074c8
streino <streino@disroot.org>
2022-05-18 01:23:02 +0200

    Remove dataset files from download.sh (now handled by TFDS)

--- download.sh
+++ download.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
+# make sure the release is in sync with the 'off_categories' dataset version.
+
 mkdir -p data
 cd data
 
 RELEASE='dataset-category-2021-09-15'
-LANG='xx'
 wget https://github.com/openfoodfacts/openfoodfacts-ai/releases/download/${RELEASE}/categories.full.json
-wget https://github.com/openfoodfacts/openfoodfacts-ai/releases/download/${RELEASE}/category_${LANG}.test.jsonl.gz
-wget https://github.com/openfoodfacts/openfoodfacts-ai/releases/download/${RELEASE}/category_${LANG}.train.jsonl.gz
-wget https://github.com/openfoodfacts/openfoodfacts-ai/releases/download/${RELEASE}/category_${LANG}.val.jsonl.gz
 wget https://github.com/openfoodfacts/openfoodfacts-ai/releases/download/${RELEASE}/agribalyse_categories.txt
