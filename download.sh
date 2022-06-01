d40cfbf9b010e7a3cc4a43eb7bb5a3d760d12fd4
Hui-Guan <huiguan@yooreco.com>
2022-06-01 16:47:13 +0200

    load category_taxonomy.json as a graph and use it to train model

--- download.sh
+++ download.sh
@@ -8,3 +8,4 @@ cd data
 RELEASE='dataset-category-2021-09-15'
 wget https://github.com/openfoodfacts/openfoodfacts-ai/releases/download/${RELEASE}/categories.full.json
 wget https://github.com/openfoodfacts/openfoodfacts-ai/releases/download/${RELEASE}/agribalyse_categories.txt
+wget https://github.com/openfoodfacts/robotoff-models/releases/download/keras-category-classifier-xx-2.0/category_taxonomy.json
