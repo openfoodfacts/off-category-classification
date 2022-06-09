f7170450b0aa29f75a351809f0b1c50663fd8038
streino <streino@disroot.org>
2022-06-09 19:02:26 +0200

    WIP: Save labels vocabulary as tf.saved_model.Asset

--- lib/io.py
+++ lib/io.py
@@ -1,28 +1,29 @@
 import pathlib
+import tempfile
 from typing import List
 
 import tensorflow as tf
 
 
 def export_model(
-        model: tf.keras.Model,
         path: pathlib.Path,
-        # label_vocab: List,
+        model: tf.keras.Model,
+        labels_vocab: List[str],
         serving_func: tf.function = None,
         **kwargs):
     """
-    Save the model with an optional custom serving function.
+    Save the model and labels, with an optional custom serving function.
 
     Parameters
     ----------
-    model: tf.keras.Model
-        Keras model instance to be saved.
-
     path: pathlib.Path
         Path where the model will be saved.
 
-    label_vocab: List[str]
-        Label vocabulary
+    model: tf.keras.Model
+        Keras model instance to be saved.
+
+    labels_vocab: List[str]
+        Label vocabulary.
 
     serving_func: tf.function, optional
         Custom serving function.
@@ -31,6 +32,12 @@ def export_model(
     **kwargs: dict, optional
         Additional keyword arguments passed to `tf.keras.Model.save`.
     """
+    tmp_dir = tempfile.TemporaryDirectory()
+    labels_path = pathlib.Path(tmp_dir.name).joinpath('labels_vocab.txt')
+    with labels_path.open('w') as w:
+        w.writelines([f"{label}\n" for label in labels_vocab])
+    model.labels_file = tf.saved_model.Asset(str(labels_path))
+
     signatures = None
     if serving_func:
         arg_specs, kwarg_specs = model.save_spec()
@@ -38,3 +45,6 @@ def export_model(
         signatures = {'serving_default': concrete_func}
 
     model.save(path, signatures=signatures, **kwargs)
+
+    # must occur after model.save, so Asset source is still around for save
+    tmp_dir.cleanup()
