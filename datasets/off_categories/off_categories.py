27a1d145e2f38011b9e16fa57e5c5a95975605ce
Raphaël Bournhonesque <raphael@bournhonesque.eu>
2023-01-04 14:18:08 +0100

    don't keep untaxonomized taxonomy or ingredient

--- datasets/off_categories/off_categories.py
+++ datasets/off_categories/off_categories.py
@@ -1,18 +1,21 @@
-from collections import namedtuple
 from dataclasses import dataclass
+import functools
 import gzip
 import orjson
-from typing import Any, Dict
+from typing import Any, Callable, Dict, Optional
 
 import tensorflow as tf
 import tensorflow_datasets as tfds
 
+from lib.taxonomy import Taxonomy, get_taxonomy
+
 
 @dataclass
 class Feature:
-  spec: tfds.features.FeatureConnector
-  default_value: Any
-  input_field: str = None  # use feature name if None
+    spec: tfds.features.FeatureConnector
+    default_value: Any
+    input_field: str = None  # use feature name if None
+    transform: Optional[Callable] = None
 
 
 _DESCRIPTION = """
@@ -22,64 +25,85 @@ More info at:
 https://openfoodfacts.org/data/dataforgood2022/big/predict_categories_dataset_documentation.txt
 """
 
-_RELEASE_NOTES = {
-  '2.0.0': 'DataForGood 2022 dataset',
-  '1.0.0': 'Initial release'
-}
+_RELEASE_NOTES = {"2.0.0": "DataForGood 2022 dataset", "1.0.0": "Initial release"}
 
 # Don't forget to run `tfds build --register_checksums` when changing the data source
-_DATA_URL = 'https://openfoodfacts.org/data/dataforgood2022/big/predict_categories_dataset_products.jsonl.gz'
+_DATA_URL = "https://openfoodfacts.org/data/dataforgood2022/big/predict_categories_dataset_products.jsonl.gz"
+
+
+def filter_untaxonomized_values(value_tags: list[str], taxonomy: Taxonomy) -> list:
+    return [value_tag for value_tag in value_tags if value_tag in taxonomy]
+
+
+category_taxonomy = get_taxonomy("category")
+ingredient_taxonomy = get_taxonomy("ingredient")
 
 _FEATURES = {
-  'code': Feature(tfds.features.Tensor(shape=(), dtype=tf.string), ''),
-  'product_name': Feature(tfds.features.Tensor(shape=(), dtype=tf.string), ''),
-  'ingredients_tags': Feature(tfds.features.Tensor(shape=(None,), dtype=tf.string), []),
-  'categories_tags': Feature(tfds.features.Tensor(shape=(None,), dtype=tf.string), [])
+    "code": Feature(tfds.features.Tensor(shape=(), dtype=tf.string), default_value=""),
+    "product_name": Feature(
+        tfds.features.Tensor(shape=(), dtype=tf.string), default_value=""
+    ),
+    "ingredients_tags": Feature(
+        tfds.features.Tensor(shape=(None,), dtype=tf.string),
+        default_value=[],
+        transform=functools.partial(
+            filter_untaxonomized_values, taxonomy=ingredient_taxonomy
+        ),
+    ),
+    "categories_tags": Feature(
+        tfds.features.Tensor(shape=(None,), dtype=tf.string),
+        default_value=[],
+        transform=functools.partial(
+            filter_untaxonomized_values, taxonomy=category_taxonomy
+        ),
+    ),
 }
 
-_LABEL = 'categories_tags'
+_LABEL = "categories_tags"
 
 
 class OffCategories(tfds.core.GeneratorBasedBuilder):
-  VERSION = tfds.core.Version('2.0.0')
-  RELEASE_NOTES = _RELEASE_NOTES
+    VERSION = tfds.core.Version("2.0.0")
+    RELEASE_NOTES = _RELEASE_NOTES
 
-  def _info(self) -> tfds.core.DatasetInfo:
-    return tfds.core.DatasetInfo(
-        builder=self,
-        description=_DESCRIPTION,
-        features=tfds.features.FeaturesDict({k: f.spec for k, f in _FEATURES.items()}),
-        supervised_keys=({k: k for k in _FEATURES.keys() if k != _LABEL}, _LABEL),
-        # Shuffle is deterministic as long as the split names stay the same
-        # (split_name is used internally as hashing salt in the shuffler)
-        disable_shuffling=False,
-        homepage='https://github.com/openfoodfacts/off-category-classification'
-    )
+    def _info(self) -> tfds.core.DatasetInfo:
+        return tfds.core.DatasetInfo(
+            builder=self,
+            description=_DESCRIPTION,
+            features=tfds.features.FeaturesDict(
+                {k: f.spec for k, f in _FEATURES.items()}
+            ),
+            supervised_keys=({k: k for k in _FEATURES.keys() if k != _LABEL}, _LABEL),
+            # Shuffle is deterministic as long as the split names stay the same
+            # (split_name is used internally as hashing salt in the shuffler)
+            disable_shuffling=False,
+            homepage="https://github.com/openfoodfacts/off-category-classification",
+        )
 
-  def _split_generators(self, dl_manager: tfds.download.DownloadManager):
-    # Downloads the data and defines the splits
-    paths = dl_manager.download({
-      'train': _DATA_URL
-    })
-    return {s: self._generate_examples(p) for s, p in paths.items()}
+    def _split_generators(self, dl_manager: tfds.download.DownloadManager):
+        # Downloads the data and defines the splits
+        paths = dl_manager.download({"train": _DATA_URL})
+        return {s: self._generate_examples(p) for s, p in paths.items()}
 
-  def _generate_examples(self, path):
-    # Yields (key, example) tuples from the dataset
-    for i, item in enumerate(OffCategories._read_json(path)):
-      if _LABEL not in item:
-        continue
-      features = {k: OffCategories._get_feature(item, k, f) for k, f in _FEATURES.items()}
-      yield i, features
+    def _generate_examples(self, path):
+        # Yields (key, example) tuples from the dataset
+        for i, item in enumerate(OffCategories._read_json(path)):
+            if _LABEL not in item:
+                continue
+            features = {
+                k: OffCategories._get_feature(item, k, f) for k, f in _FEATURES.items()
+            }
+            yield i, features
 
-  @staticmethod
-  def _get_feature(item: Dict, name: str, feature: Feature):
-    field = feature.input_field if feature.input_field else name
-    return item.get(field, feature.default_value)
+    @staticmethod
+    def _get_feature(item: Dict, name: str, feature: Feature):
+        field = feature.input_field if feature.input_field else name
+        return item.get(field, feature.default_value)
 
-  @staticmethod
-  def _read_json(path):
-    with gzip.open(path, 'rt', encoding='utf-8') as f:
-      for line in f:
-        l = line.strip()
-        if l:
-          yield orjson.loads(l)
+    @staticmethod
+    def _read_json(path):
+        with gzip.open(path, "rt", encoding="utf-8") as f:
+            for line in f:
+                l = line.strip()
+                if l:
+                    yield orjson.loads(l)
