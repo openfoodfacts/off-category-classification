fix computation of val and test metrics on end of training

train.py
@@ -671,7 +671,9 @@ def main(
     PREDICTION_DIR = MODEL_DIR / "predictions"
     PREDICTION_DIR.mkdir()
     for split_name, split_command in (("val", VAL_SPLIT), ("test", TEST_SPLIT)):
-        split_ds = load_dataset("off_categories", split=split_command)
+        split_ds = load_dataset(
+            "off_categories", split=split_command, features=features
+        )
         preds = m.predict(split_ds.padded_batch(config.batch_size))
         np.save(PREDICTION_DIR / split_name, preds, allow_pickle=False)
         # This is the function exported as the default serving function in our saved model
@@ -704,7 +706,13 @@ def main(
         best_model_metrics = {
             f"epoch/{split_name}_{metric_name}": value
             for metric_name, value in m.evaluate(
-                split_ds.padded_batch(config.batch_size), return_dict=True
+                load_dataset(
+                    "off_categories",
+                    split=split_command,
+                    features=features,
+                    as_supervised=True,
+                ).padded_batch(config.batch_size),
+                return_dict=True,
             ).items()
         }
         print("[red]best model metrics[/red]")